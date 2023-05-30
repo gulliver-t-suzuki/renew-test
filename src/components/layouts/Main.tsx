0f0f28fb565aa2881ad0efa788f0abbdc5fcc329
t-suzuki <gulliver.t.suzuki@gmail.com>
2023-05-30 16:03:12 +0900

    ✨ テキストついてくるやつをパワーアップ

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -5,28 +5,51 @@ const Main = () => {
   useEffect(() => {
     const handleScroll = () => {
       const h1Element = document.querySelector("h1");
+      const div = document.getElementById("bottomContents");
       const y = window.pageYOffset;
-      if (h1Element) {
-        const h1Height = h1Element.getBoundingClientRect().height;
+      const windowHeight = document.documentElement.clientHeight;
 
-        if (y >= 255 + h1Height) {
+      //NULLチェック
+      if (h1Element && div) {
+        const h1Height = h1Element.getBoundingClientRect().height;
+        const h1Position = windowHeight - (h1Height + 80); //h1の位置:どれだけ離すか次第,CSSと連動　*0.1はbottom: 10%
+        const divPosition = div.getBoundingClientRect().top + y; //divの位置
+        const changePosition = divPosition - h1Position; //固定かどうかが変わる位置
+        console.log("h1の位置: ", h1Position);
+        console.log("divの位置: ", divPosition);
+        console.log("切り替えするスクロール量: ", changePosition);
+
+        //現在の場所を確認して固定するかしないか判断
+        if (y >= changePosition) {
           h1Element.classList.remove(styles.isFixed);
           h1Element.classList.add(styles.isAbsolute);
-        } else if (y < 255 + h1Height) {
+        } else if (y < changePosition) {
           h1Element.classList.remove(styles.isAbsolute);
           h1Element.classList.add(styles.isFixed);
         }
+        console.log("div：" + div.getBoundingClientRect().top);
+        console.log("h1：" + h1Element.getBoundingClientRect().top);
+        console.log("スクロール量：" + y);
       } else {
       }
     };
 
     const h1Element = document.querySelector("h1");
+    const div = document.getElementById("bottomContents");
     const y = window.pageYOffset;
-    if (h1Element) {
-      if (y >= 412) {
+    const windowHeight = document.documentElement.clientHeight;
+
+    //NULLチェック
+    if (h1Element && div) {
+      const h1Height = h1Element.getBoundingClientRect().height;
+      const h1Position = windowHeight - (h1Height + 80); //h1の位置:どれだけ離すか次第,CSSと連動　*0.1はbottom: 10%
+      const divPosition = div.getBoundingClientRect().top + y; //divの位置
+      const changePosition = divPosition - h1Position; //固定かどうかが変わる位置
+      //現在の場所を確認して固定するかしないか判断
+      if (y >= changePosition) {
         h1Element.classList.remove(styles.isFixed);
         h1Element.classList.add(styles.isAbsolute);
-      } else if (y < 412) {
+      } else if (y < changePosition) {
         h1Element.classList.remove(styles.isAbsolute);
         h1Element.classList.add(styles.isFixed);
       }
